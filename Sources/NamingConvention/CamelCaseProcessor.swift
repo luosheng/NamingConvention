a6e131729fa06a9ae7955331a3448f9a709cad53
Sheng Luo <47009+luosheng@users.noreply.github.com>
2023-11-16 08:48:21 +0800

    Implement camel case processor

--- Sources/NamingConvention/CamelCaseProcessor.swift
+++ Sources/NamingConvention/CamelCaseProcessor.swift
@@ -1,13 +1,26 @@
+import Foundation
+
 final class CamelCaseProcessor: Processor {
     func match(text: String) -> Bool {
-        return false
+        let regex = try! NSRegularExpression(pattern: "^[a-z][a-z0-9A-Z]*")
+        let result = regex.firstMatch(
+            in: text, range: NSRange(location: 0, length: text.utf16.count))
+        return result != nil
     }
 
     func segmented(text: String) -> [String] {
-        return []
+        let regex = try! NSRegularExpression(pattern: "[a-z]+|[A-Z][a-z]*")
+        let matches = regex.matches(
+            in: text, range: NSRange(location: 0, length: text.utf16.count))
+        return matches.map {
+            let range = Range($0.range, in: text)!
+            return String(text[range]).lowercased()
+        }
     }
 
     func connected(segments: [String]) -> String {
-        return ""
+        let head = segments.first
+        let tail = segments.dropFirst()
+        return (head?.lowercased() ?? "") + tail.map { $0.capitalized }.joined()
     }
 }
